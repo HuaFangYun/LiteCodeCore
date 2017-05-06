cc0e1f1ff78b76db7a1a918560c457439c8d9521
luckearth <luckearth@luckearth.cn>
2017-05-06 22:38:09 +0800

    增加WebSocket

---
+++ src/LuckyCode.Core/WebSocket/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LuckyCode.Core.WebSocket
+{
+    public static class Extensions
+    {
+        public static IApplicationBuilder MapWebSocketManager(this IApplicationBuilder app, PathString path, WebSocketHandler handler)
+        {
+            return app.Map(path, (_app) => _app.UseMiddleware<WebSocketMiddleware>(handler));
+        }
+
+        public static IServiceCollection AddWebSocketManager(this IServiceCollection services)
+        {
+            var handlerBaseType = typeof(WebSocketHandler);
+
+            foreach (var type in Assembly.GetEntryAssembly().ExportedTypes)
+            {
+                if (type.GetTypeInfo().BaseType == handlerBaseType)
+                {
+                    services.AddSingleton(type);
+                }
+            }
+
+            return services;
+        }
+    }
+}
