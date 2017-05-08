4316da6ce903d031b6d7b5c0660b9955eb9f835b
luckearth <luckearth@luckearth.cn>
2017-05-08 10:15:33 +0800

    增加 websocket 配置

--- src/LuckyCode.WebSite/Startup.cs
+++ src/LuckyCode.WebSite/Startup.cs
@@ -7,8 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
-using LiteCode.Service;
-using LiteCode.ViewModels.Mapper;
+
 using LuckyCode.Core.WebSocket;
 using LuckyCode.Core.WebSocketChat;
 using LuckyCode.Core.Data;
