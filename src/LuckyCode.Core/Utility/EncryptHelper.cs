084bd14fe4f42a74922a58b0f44e0f314a745e3b
luckearth <luckearth@luckearth.cn>
2017-05-17 16:36:45 +0800

    修复Aes 解密算法

--- src/LuckyCode.Core/Utility/EncryptHelper.cs
+++ src/LuckyCode.Core/Utility/EncryptHelper.cs
@@ -62,17 +62,11 @@ namespace LuckyCode.Core.Utility
                         {
                             swEncrypt.Write(input);
                         }
-
                         var iv = aesAlg.IV;
-
                         var decryptedContent = msEncrypt.ToArray();
-
                         var result = new byte[iv.Length + decryptedContent.Length];
-
                         Buffer.BlockCopy(iv, 0, result, 0, iv.Length);
-                        Buffer.BlockCopy(decryptedContent, 0, result,
-                            iv.Length, decryptedContent.Length);
-
+                        Buffer.BlockCopy(decryptedContent, 0, result, iv.Length, decryptedContent.Length);
                         return Convert.ToBase64String(result);
                     }
                 }
@@ -84,26 +78,23 @@ namespace LuckyCode.Core.Utility
         /// <param name="input"></param>
         /// <param name="key"></param>
         /// <returns></returns>
-        public static string AESDecrypt(string input, string key)
+        public static string DecryptString(string cipherText, string keyString)
         {
-            var fullCipher = Convert.FromBase64String(input);
-
-            var iv = new byte[16];
-            var cipher = new byte[16];
-
+            var fullCipher = Convert.FromBase64String(cipherText);
+            byte[] iv = new byte[16];
+            var cipher = new byte[fullCipher.Length - iv.Length];
             Buffer.BlockCopy(fullCipher, 0, iv, 0, iv.Length);
-            Buffer.BlockCopy(fullCipher, iv.Length, cipher, 0, iv.Length);
-            var decryptKey = Encoding.UTF8.GetBytes(key);
+            Buffer.BlockCopy(fullCipher, iv.Length, cipher, 0, fullCipher.Length - iv.Length);
+            var key = Encoding.UTF8.GetBytes(keyString);
 
             using (var aesAlg = Aes.Create())
             {
-                using (var decryptor = aesAlg.CreateDecryptor(decryptKey, iv))
+                using (var decryptor = aesAlg.CreateDecryptor(key, iv))
                 {
                     string result;
                     using (var msDecrypt = new MemoryStream(cipher))
                     {
-                        using (var csDecrypt = new CryptoStream(msDecrypt,
-                            decryptor, CryptoStreamMode.Read))
+                        using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
                         {
                             using (var srDecrypt = new StreamReader(csDecrypt))
                             {
